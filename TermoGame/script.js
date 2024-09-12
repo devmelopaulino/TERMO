bfcf06f9d94246b22aec552053f66db43b6d7145
DevMelo <devmelopaulino@gmail.com>
2024-09-12 16:38:53 -0300

    refactoring code

--- TermoGame/script.js
+++ TermoGame/script.js
@@ -1,4 +1,4 @@
-class GrindButto{
+class GrindButto {
     constructor() {
 
     }
@@ -9,21 +9,34 @@ let lastButtonSelect = null;
 
 let gridButtons = document.querySelectorAll('.grid-item');
 
-gridButtons.forEach(function(button) {
-    button.addEventListener('click', function() {      
-        if(lastButtonSelect != null)
-        {
-            lastButtonSelect.classList.remove('grid-item-select');
-            lastButtonSelect.classList.add('grid-item');
-        }
-        lastButtonSelect = button;
-        button.classList.remove('grid-item');
-        button.classList.add('grid-item-select');
+gridButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+        SelectButton(button);
     });
 
-    button.addEventListener('keydown', function(event) {
-        console.log('Tecla pressionada:', event.key); 
+    button.addEventListener('keydown', function (event) {
+        console.log(`${event.key}`);
     });
 
 });
+
+function SelectButton(button) {
+    if (lastButtonSelect) {
+        ChangeButtonToUnselect(lastButtonSelect);
+    }
+    ChangeButtonToSelect(button);
+}
+
+function ChangeButtonToSelect(button)
+{
+    
+    lastButtonSelect = button;
+    button.classList.add('grid-item-select');
+    button.classList.remove('grid-item');
+}
+function ChangeButtonToUnselect(button)
+{
+    button.classList.remove('grid-item-select');
+    button.classList.add('grid-item');
+}
 
